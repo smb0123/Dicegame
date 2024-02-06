f3bdc06e717976feb80fe94884a6e8b679e884d9
minbo <smb5174@naver.com>
2024-02-06 11:45:59 +0900

    feat: css의 class를 사용해서 컴포넌트에 디자인 적용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Board from "./Board.js";
-import Button from './Button';
 import { useState } from 'react';
+import Board from './Board';
+import Button from './Button';
+import './App.css';
 
 function random(n) {
   return Math.ceil(Math.random() * n);
@@ -15,7 +16,7 @@ function App() {
     const nextOtherNum = random(6);
     setMyHistory([...myHistory, nextMyNum]);
     setOtherHistory([...otherHistory, nextOtherNum]);
-  }
+  };
 
   const handleClearClick = () => {
     setMyHistory([]);
@@ -23,17 +24,19 @@ function App() {
   };
 
   return (
-    <div>
-      <div>
-        <Button color="blue" onClick={handleRollClick}>던지기</Button>
-        <Button color="red" onClick={handleClearClick}>처음부터</Button>
-      </div>
+    <div className="App">
+      <Button className="App-button" color="blue" onClick={handleRollClick}>
+        던지기
+      </Button>
+      <Button className="App-button" color="red" onClick={handleClearClick}>
+        처음부터
+      </Button>
       <div>
         <Board name="나" color="blue" gameHistory={myHistory} />
-        <Board name="상대" color="red" gameHistory={otherHistory}/>
+        <Board name="상대" color="red" gameHistory={otherHistory} />
       </div>
     </div>
-  ); 
+  );
 }
 
 export default App;
